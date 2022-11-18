94b4a786f9f8df91b9ff13ebc3903783a40df610
hosizoraru <hosizoraru@gmail.com>
2022-11-18 00:11:50 +0000

    qwq

--- demo.py
+++ demo.py
@@ -1,1 +1,10 @@
-print('hello,world!')
+s = input()
+month = int(s[4:6])
+if month in [1,2,3]:
+    print('spring')
+elif month in [4,5,6]:
+    print('summer')
+elif month in [7,8,9]:
+    print('autumn')
+elif month in [10,11,12]:
+    print('winter')
